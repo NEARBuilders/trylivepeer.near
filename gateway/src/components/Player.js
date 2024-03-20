db056a0c0b35707b00bad2089fb44520f67d84d0
stefano <stefano1990@gmail.com>
2024-03-20 18:21:47 +0000

    update livepeer library

--- gateway/src/components/Player.js
+++ gateway/src/components/Player.js
@@ -1,13 +1,34 @@
-import React from "react";
+import * as Player from "@livepeer/react/player";
+import { getSrc } from "@livepeer/react/external";
 
-export const Player = (props) => {
-  // import Livepeer
-  // implement agnostic component
-  // props come from widget
-  return (
-    <textarea
-      value={JSON.stringify(props, undefined, 2)}
-      style={props.style ?? { width: "100%", height: "100%" }}
-    />
-  );
+const PLAYBACK_ID = "8b3bdqjtdj4jsjwa";
+
+export const getPlaybackSource = async (playbackId = PLAYBACK_ID) => {
+	const playbackInfo = await livepeer.playback.get(playbackId);
+	const src = getSrc(playbackInfo.playbackInfo);
+	return src;
+};
+
+export const VideoPlayer = (props) => {
+	// export const DemoPlayer = ({ src }: { src: Src | null }) => {
+
+	const src = getPlaybackSource();
+
+	return (
+		<Player.Root src={src}>
+			<Player.Container>
+				<Player.Video title="Live stream" />
+				<Player.Controls className="flex items-center justify-center">
+					<Player.PlayPauseTrigger className="w-10 h-10">
+						<Player.PlayingIndicator asChild matcher={false}>
+							<PlayIcon />
+						</Player.PlayingIndicator>
+						<Player.PlayingIndicator asChild>
+							<PauseIcon />
+						</Player.PlayingIndicator>
+					</Player.PlayPauseTrigger>
+				</Player.Controls>
+			</Player.Container>
+		</Player.Root>
+	);
 };
