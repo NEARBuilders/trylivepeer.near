0a46d43a2209d4257cdde32f08d454a6340cf36c
stefano <stefano1990@gmail.com>
2024-04-05 09:12:54 +0200

    remove tailwind wrapper, added tailwind css output

--- apps/trylivepeer.near/widget/page/sandbox.jsx
+++ apps/trylivepeer.near/widget/page/sandbox.jsx
@@ -7,27 +7,19 @@ const playerData = {
 
 const broadcastData = {}
 
-const { Tailwind } = VM.require("klngrs.near/widget/Tailwind") || {
-  TailwindWrapper: () => <></>,
-};
-
 return (
   <>
     <div className="row">
       <div className="col">
         <div>
           <h1>Player</h1>
-					<Tailwind>
           	<Widget src="trylivepeer.near/widget/Player" props={playerData} />
-					</Tailwind>
         </div>
       </div>
       <div className="col">
         <div>
           <h1>Broadcast</h1>
-					<Tailwind>
           	<Widget src="trylivepeer.near/widget/Broadcast" props={broadcastData} />
-					</Tailwind>
         </div>
       </div>
     </div>
