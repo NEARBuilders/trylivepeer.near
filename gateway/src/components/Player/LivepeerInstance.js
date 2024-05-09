06763f61a4e22de0e29632ba7613958d8af21dfa
stefano <stefano1990@gmail.com>
2024-05-09 15:56:43 +0200

    Remove some comments

--- gateway/src/components/Player/LivepeerInstance.js
+++ gateway/src/components/Player/LivepeerInstance.js
@@ -5,12 +5,10 @@ const API_KEY = process.env.REACT_APP_LIVEPEER_STUDIO_API_KEY;
 let livepeerInstance = null;
 
 function createLivepeerInstance(apiKey = API_KEY) {
-  console.log("-- HERE");
   if (!livepeerInstance) {
     livepeerInstance = new Livepeer({
       apiKey,
     });
-    console.log("Livepeer instance created");
   } else {
     console.log("Returning existing Livepeer instance");
   }
@@ -18,9 +16,7 @@ function createLivepeerInstance(apiKey = API_KEY) {
 }
 
 function updateLivepeerInstance(apiKey) {
-  console.log("-- updating livepeer instance");
   livepeerInstance = new Livepeer({ apiKey });
-  console.log("Livepeer instance updated with new API key: ", apiKey);
   return livepeerInstance;
 }
 
